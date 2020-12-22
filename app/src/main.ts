b452b01696c4e3b0fa75e1dc5f28c9c7977112e8
Gabriel Martins <gabrielgmartinsbr@gmail.com>
2020-12-22 05:23:10 -0300

    chore(web-app): add typescript and eslint

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-import App from './App.vue'
+// import App from './App.vue'
+import App from '@/App.vue'
 
-import './styles/styles.scss';
+import './styles/styles.scss'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
