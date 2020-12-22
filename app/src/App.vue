9e7437c70a156133465b2e8271bed6e95e442642
Gabriel Martins <gabrielgmartinsbr@gmail.com>
2020-12-22 05:03:39 -0300

    feat(web-app): add colored background

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,22 +1,13 @@
 <template>
   <div id="app">
+    <div class="text-center">
+      <h2>Welcome!</h2>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
