688ae323f294a647e6199d4b7bf90830a9e7a3ec
Gabriel Martins <gabrielgmartinsbr@gmail.com>
2020-12-22 08:40:34 -0300

    feat(web-app): add sign in form validation

--- app/src/views/sign/SignInView.vue
+++ app/src/views/sign/SignInView.vue
@@ -1,30 +1,61 @@
 <template>
   <div class="sign-in-view">
     <h3 class="form-title">Access account</h3>
-    <hr>
-    <b-form novalidate>
-      <b-form-group label="Email or username">
-        <b-form-input type="text" />
-      </b-form-group>
-
-      <b-form-group label="Password">
-        <b-form-input type="password" />
-      </b-form-group>
-
-      <div class="form-actions">
-        <div>
-          <div class="mb-1">
-            <small>Don't have an account?</small>
-          </div>
-          <b-button variant="outline-dark" @click="signUp()">
-            Create one now
-          </b-button>
-        </div>
-        <b-button variant="orange" size="lg" class="ml-auto my-3"
-          >Sign in</b-button
+    <hr />
+    <!-- Sign In Form -->
+    <ValidationObserver ref="form">
+      <b-form @submit="onSubmit" novalidate>
+        <!-- Username/Email Field -->
+        <ValidationProvider
+          v-slot="v"
+          name="email/username"
+          rules="req|min:4|max:64"
         >
-      </div>
-    </b-form>
+          <b-form-group
+            label="Email or username"
+            :invalid-feedback="v.errors[0]"
+          >
+            <b-form-input
+              type="text"
+              v-model="model.username"
+              :state="validated ? v.valid : null"
+            />
+          </b-form-group>
+        </ValidationProvider>
+
+        <!-- Password Field -->
+        <ValidationProvider v-slot="v" name="password" rules="req|min:4|max:32">
+          <b-form-group label="Password" :invalid-feedback="v.errors[0]">
+            <b-form-input
+              type="password"
+              v-model="model.password"
+              :state="validated ? v.valid : null"
+            />
+          </b-form-group>
+        </ValidationProvider>
+
+        <!-- Form Actions Section -->
+        <div class="form-actions">
+          <div>
+            <div class="mb-1">
+              <small>Don't have an account?</small>
+            </div>
+            <!-- Sign Up Button -->
+            <b-button variant="outline-dark" @click="signUp()">
+              Create one now
+            </b-button>
+          </div>
+          <!-- Submit Button -->
+          <b-button
+            variant="orange"
+            size="lg"
+            class="ml-auto my-3"
+            type="submit"
+            >Sign in</b-button
+          >
+        </div>
+      </b-form>
+    </ValidationObserver>
   </div>
 </template>
 
@@ -47,10 +78,35 @@
 </style>
 
 <script>
+import { ValidationObserver, ValidationProvider } from "vee-validate";
+
 export default {
+  components: { ValidationObserver, ValidationProvider },
+  data() {
+    return {
+      validated: false,
+      model: {
+        username: "",
+        password: "",
+      },
+    };
+  },
   methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.signIn();
+    },
     signUp() {
       this.$router.push({ name: "SignUpView" });
+    },
+    async signIn() {
+      const valid = await this.$refs.form.validate();
+      this.$data.validated = true;
+      if (!valid) {
+        return;
+      }
+      const model = this.model;
+      console.log({ login: model });
     },
   },
 };
